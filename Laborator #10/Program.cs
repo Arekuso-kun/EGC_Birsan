dccb450f71a58790dc201247ea07b8671c0346ad
Arekuso-kun <alexodrino@gmail.com>
2024-01-27 02:02:09 +0200

    Laborator #10

---
+++ Laborator #10/Program.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// ======================
+// Laborator #10
+// Bîrsan Dorin-Alexandru
+// grupa 3132a
+// ======================
+
+namespace Laborator__10
+{
+    static class Program {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
